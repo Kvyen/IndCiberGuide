39abf7f7f76347a3205a170e5ad109f329f1b24c
kvyen <kuyenrayrupayan@gmail.com>
2025-11-14 19:04:50 -0300

    Me faltaba el libro

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         Biblioteca biblioteca = new Biblioteca();
 
-        biblioteca.agregarRecurso(new Libro("Cien años de soledad", "García Márquez", 1967));
+        biblioteca.agregarRecurso(new Libro("Manual de Seguridad Digital", "Marcos Robledo Hoecker", 2018));
         biblioteca.agregarRecurso(new Guia("guia sobre ciberseguridad", "WKI", 2025, "Seguridad Online"));
         biblioteca.agregarRecurso(new Video("Tutorial VPN", "Kvyen", 2025, 5, "Youtube"));
 
