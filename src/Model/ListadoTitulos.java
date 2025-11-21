a2af3d1cd165ca3a73beb6ac51f3299fed681d08
kvyen <kuyenrayrupayan@gmail.com>
2025-11-21 20:50:55 -0300

    Actualizacion extra

--- src/Model/ListadoTitulos.java
+++ src/Model/ListadoTitulos.java
@@ -8,11 +8,11 @@ public class ListadoTitulos {
     private List<String> titulos = new ArrayList<>();
 
     public ListadoTitulos() {
-        titulos.add("Introducción a la Ciberseguridad");
-        titulos.add("Fundamentos de Protección Digital");
-        titulos.add("Guía de Privacidad para Jóvenes");
-        titulos.add("Buenas Prácticas en Internet");
-        titulos.add("Conceptos Básicos de Ciberseguridad");
+        titulos.add("Introducción a la Ciberseguridad (https://teletrabajo.anuies.mx/wp-content/uploads/2020/05/Introduccion_ala_ciberseguridad_v2.pdf?utm_source=)");
+        titulos.add("Fundamentos de Protección Digital (https://cens.cl/wp-content/uploads/2024/08/Guia-Privacidad-Datos-CENS.pdf?utm_source=)");
+        titulos.add("Guía de Privacidad para Jóvenes (https://www.aepd.es/guias/guia-privacidad-y-seguridad-en-internet.pdf)");
+        titulos.add("Buenas Prácticas en Internet (https://www.derechosdigitales.org/wp-content/uploads/Gu%C3%ADa-de-Buenas-Pr%C3%A1cticas-Final.pdf?utm_source=)");
+        titulos.add("Conceptos Básicos de Ciberseguridad (https://www.ftc.gov/system/files/attachments/conceptos-basicos-de-ciberseguridad/cybersecurity_sb_cyber-basics-es.pdf?utm_source=)");
     }
 
     public List<String> getTitulos() {
